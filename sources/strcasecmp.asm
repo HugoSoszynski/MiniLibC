e86ab9d8d9d447c2f7c3f2596ec1309a28b936f2
Hugo SOSZYNSKI <hugo.soszynski@epitech.eu>
2017-03-22 21:52:23 +0100

    strcasecmp (soszyn_h & loens_g are stupid)

--- sources/strcasecmp.asm
+++ sources/strcasecmp.asm
@@ -8,6 +8,7 @@ global	strcasecmp
 strcasecmp:
 	xor rax, rax
 	xor r11, r11
+	xor rdx, rdx
 	jmp .loop
 
 .inc:
@@ -19,12 +20,23 @@ strcasecmp:
 	cmp byte[rsi + r11], 0
 	jz  .exit_success
 	mov al, byte[rdi + r11]
-	sub al, byte[rsi + r11]
+	mov dl, byte[rsi + r11]
+	cmp al, 'A'
+	jl  .lower1
+	cmp al, 'Z'
+	jg  .lower1
+	add al, 32
+	
+.lower1:
+	cmp dl, 'A'
+        jl  .lower
+        cmp dl, 'Z'
+        jg  .lower
+        add dl, 32
+
+.lower:
+	sub al, dl
 	cmp al, 0
-	jz .inc
-	cmp al, 32
-	jz .inc
-	cmp al, -32
 	jz .inc
 
 .exit_failure:
